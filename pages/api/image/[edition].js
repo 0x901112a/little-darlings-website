ca1ccdb19831d2a5a69cb6a302713ae923aad1d9
golira <golira.eth@gmail.com>
2024-03-29 09:57:36 +0900

    Testing different rpc provider?

--- pages/api/image/[edition].js
+++ pages/api/image/[edition].js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   try {
     if (editionNumber + 1 > lastMintedToken) {
       const provider = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-        ? new providers.AlchemyProvider("mainnet", process.env.NEXT_PUBLIC_ALCHEMY_API_KEY)
+        ? new ethers.providers.JsonRpcProvider(`https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`)
         : new ethers.providers.JsonRpcProvider(`https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`)
       const contract = new ethers.Contract(address, LittleDarlingsABI, provider)
       const totalSupply = await contract.totalSupply().then(token => token.toNumber())
